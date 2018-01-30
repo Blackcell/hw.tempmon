9a2c8dc7c68c068a457ef003b752758abfc3334d
Bobby Gaylor <bobby.gaylor@hotmail.com>
2018-01-30 13:31:02 -0600

    Initial Commit

---
+++ config.py
@@ -0,0 +1,4 @@
+api = {'location_id': '633e5690-0537-11e8-98bf-4bee47882831',
+         'url_store': 'http://crypto/api/temperature/store',
+         'url_name': 'http://crypto/api/location/name',
+         'api_secret': '47d7b86c-3275-4cd3-9c88-ddb96e405e98'}
---
+++ tempmon.py
@@ -0,0 +1,116 @@
+"""
+Thanks to this project for base methods to read the temperature
+https://github.com/timfernando/temperature-serve-pi/blob/master/temperature.py
+"""
+
+from os import listdir, system
+import sys
+import json
+import config as cfg
+import requests
+import socket
+
+TEMPMON_VERSION = 0.1
+
+API_SECRET = cfg.api['api_secret']
+API_URL_STORE = cfg.api['url_store']
+API_URL_NAME = cfg.api['url_name']
+LOCATION_ID = cfg.api['location_id']
+
+DEVICE_FOLDER = "/sys/bus/w1/devices/"
+DEVICE_SUFFIX = "/w1_slave"
+WAIT_INTERVAL = 0.2
+
+# system('modprobe w1-gpio')
+# system('modprobe w1-therm')
+
+print('\n\n\n')
+print('#############################')
+print("TempMon v" + str(TEMPMON_VERSION))
+print('#############################')
+
+
+def main():
+    print("DEVICE")
+    print(socket.gethostname() + '\n')
+    print("LOCATION ID")
+    print(LOCATION_ID + '\n')
+    print("LOCATION NAME")
+    location_name = get_location_name()
+    print(location_name)
+    print('#############################')
+
+
+
+
+def get_location_name():
+
+    response = requests.post(API_URL_NAME, data={
+        'api_secret': API_SECRET,
+        'location_id': LOCATION_ID
+    })
+
+    return response.text
+
+
+def store():
+
+    response = requests.post(API_URL_STORE, data={
+        'api_secret': API_SECRET,
+        'location_id': LOCATION_ID,
+        'celsius': 4,
+        'fahrenheit': 40
+    })
+
+    print(response.text)
+
+
+def guess_temperature_sensor():
+    # Try guessing the location of the installed temperature sensor
+
+    devices = listdir(DEVICE_FOLDER)
+    devices = [device for device in devices if device.startswith('28-')]
+    if devices:
+        # print "Found", len(devices), "devices which maybe temperature sensors."
+        return DEVICE_FOLDER + devices[0] + DEVICE_SUFFIX
+    else:
+        sys.exit("Sorry, no temperature sensors found")
+
+
+def raw_temperature(device):
+    # Get a raw temperature reading from the temperature sensor
+
+    raw_reading = None
+    with open(device, 'r') as sensor:
+        raw_reading = sensor.readlines()
+    return raw_reading
+
+
+def read_temperature(device):
+    # Keep trying to read the temperature from the sensor until it returns a valid result
+
+    lines = raw_temperature(device)
+
+    # Keep retrying till we get a YES from the thermometer
+    # 1. Make sure that the response is not blank
+    # 2. Make sure the response has at least 2 lines
+    # 3. Make sure the first line has a "YES" at the end
+    while not lines and len(lines) < 2 and lines[0].strip()[-3:] != 'YES':
+        # If we haven't got a valid response, wait for the WAIT_INTERVAL
+        # (seconds) and try again.
+        time.sleep(WAIT_INTERVAL)
+        lines = raw_temperature()
+
+    # Split out the raw temperature number
+    temperature = lines[1].split('t=')[1]
+
+    # Check that the temperature is not invalid
+    if temperature != -1:
+        temperature_celsius = round(float(temperature) / 1000.0, 1)
+        temperature_fahrenheit = round((temperature_celsius * 1.8) + 32.0, 1)
+
+    response = {'celsius': temperature_celsius,
+                'fahrenheit': temperature_fahrenheit}
+    return response
+
+main()
