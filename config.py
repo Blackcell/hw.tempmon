96351d82c84ec2356bf79ef08bfc3baa59da23dc
Bobby Gaylor <bobby.gaylor.home@gmail.com>
2018-01-30 21:57:19 -0600

    Removing config from tracking

--- config.py
+++
@@ -1,4 +0,0 @@
-api = {'location_id': 'd0166420-053a-11e8-8052-f5db0d7a653b',
-         'url_store': 'https://mining.progtronics.com/api/temperature/store',
-         'url_name': 'https://mining.progtronics.com/api/location/name',
-         'api_secret': '47d7b86c-3275-4cd3-9c88-ddb96e405e98'}
