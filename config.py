8ef7be230cfa297cf5e6f2f1b5e4536ee806790d
Bobby Gaylor <bobby.gaylor.home@gmail.com>
2019-03-19 18:11:54 -0500

    Adding example config

---
+++ config.py
@@ -0,0 +1,12 @@
+api = {'location_id': '',
+         'url_store': 'url_to_save_reading',
+         'url_name': 'url_to_get_device_name',
+         'api_secret': ''}
+device = {
+        'offset_c': 0,
+        'control_pin': 21
+}
+tolerance = {
+        'max_f': 72,
+        'min_f': 70
+}
